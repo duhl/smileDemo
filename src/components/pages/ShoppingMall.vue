c53df8b3d92824fb13e4f4f0333b56a1174bdce6
duhailong <hailong.du@vcg.com>
2018-08-04 02:24:08 +0800

    searchBar is ok

--- src/components/pages/ShoppingMall.vue
+++ src/components/pages/ShoppingMall.vue
@@ -1,7 +1,20 @@
 <template>
-    <div>
-        {{msg}}
+  <div>
+    <div class="search-bar">
+      <van-rown>
+        <van-col span="3">
+          <span class="location iconfont icon-location"></span>
+        </van-col>
+        <van-col span="16">
+          <input type="text" class="search-input">
+        </van-col>
+        <van-col span="5" class="search-button">
+          <van-button size="mini">搜索</van-button>
+        </van-col>
+      </van-rown>
     </div>
+
+  </div>
 </template>
 
 <script>
@@ -15,4 +28,29 @@ export default {
 </script>
 
 <style scoped>
+.search-bar {
+  height: 2.2rem;
+  background-color: #e5017d;
+}
+.location {
+  width: 2rem;
+  height: 2.2rem;
+  line-height: 2.2rem;
+  margin-left: 0.5rem;
+  font-size: 1.8rem;
+  color: white;
+}
+.search-input {
+  width: 100%;
+  padding-top: 0.2rem;
+  height: 1.5rem;
+  background-color: transparent;
+  color: white;
+  border: 0;
+  border-bottom: 1px solid white;
+}
+.search-button {
+  line-height: 2rem;
+  padding-left: 0.8rem;
+}
 </style>
