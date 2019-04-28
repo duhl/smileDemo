b8f3290b37f3df300201ee50c90920a91abfdae7
duhailong <hailong.du@vcg.com>
2019-04-28 17:39:45 +0800

    update

---
+++ src/components/pages/Register.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <van-nav-bar
+      title="用户注册"
+      left-text="返回"
+      left-arrow
+      @click-left="goBack"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    goBack() {
+      this.$router.go(-1);
+    }
+  }
+};
+</script>
+
+<style lang="less">
+</style>
