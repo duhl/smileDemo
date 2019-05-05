12a8c18422be3ae8e98059c09ead9b8940a38691
duhailong <hailong.du@vcg.com>
2019-05-05 13:31:49 +0800

    update goods

--- src/components/pages/GoodsDetail.vue
+++ src/components/pages/GoodsDetail.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
-    <h1>商品详情</h1>
+  <div class="root">
+    <!-- <h1>商品详情</h1> -->
+    <van-nav-bar
+      title="商品详情"
+      left-text="返回"
+      left-arrow
+      @click-left="goBack"
+    />
+    <div class="topic">
+      <img :src="goodsData.IMAGE1" alt="" />
+    </div>
   </div>
 </template>
 
@@ -11,7 +20,8 @@ import url from "@/serviceAPI.js";
 export default {
   data() {
     return {
-      msg: ""
+      msg: "",
+      goodsData: {}
     };
   },
   computed: {
@@ -33,14 +43,29 @@ export default {
       })
         .then(res => {
           console.log("获取到详情数据", res);
+          if (res.data.code == 0 && res.data.data) {
+            this.goodsData = res.data.data;
+          } else {
+            Toast("服务器错误，商品详情数据获取失败");
+          }
         })
         .catch(err => {
           console.log(err);
         });
+    },
+    goBack() {
+      this.$router.go(-1);
     }
   }
 };
 </script>
 
 <style lang="less" scoped>
+.root {
+  .topic {
+    img {
+      width: 100%;
+    }
+  }
+}
 </style>
